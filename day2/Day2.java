b50955dda34b8d4ba707d40e204cc3f0fd3c1a6d
RocketsAndRedstome <dwlethgo@gmail.com>
2024-12-02 09:31:25 -0500

    attempted to add a loop to read file

--- day2/Day2.java
+++ day2/Day2.java
@@ -1,10 +1,16 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Day2 {
     public static void main(String[] args) {
         try{
-            File grid = new File("day2Input.txt");
-            Scanner userData = new Scanner(grid);
+            File numbers = new File("day2Input.txt");
+            Scanner userData = new Scanner(numbers);
+            while(userData.hasNext()){
+                String temp = userData.nextLine();
+                System.out.println(temp);
+            }
+            userData.close();
         }
         catch(FileNotFoundException e){
             System.out.println("Something is wrong with the given file name, path or something else related to the file");
